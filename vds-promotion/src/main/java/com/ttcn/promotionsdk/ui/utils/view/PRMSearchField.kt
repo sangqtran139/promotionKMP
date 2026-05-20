2f7740096453ea4a201d6c39757ebec827217a11
os_rikkei_tunght <tunght@rikkeisoft.com>
2026-05-20 09:31:30 +0700

    fix search bar default value

--- vds-promotion/src/main/java/com/ttcn/promotionsdk/ui/utils/view/PRMSearchField.kt
+++ vds-promotion/src/main/java/com/ttcn/promotionsdk/ui/utils/view/PRMSearchField.kt
@@ -9,13 +9,11 @@ import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.LinearLayout
 import androidx.annotation.ColorRes
+import androidx.core.widget.addTextChangedListener
 import com.ttcn.promotionsdk.R
 import com.ttcn.promotionsdk.databinding.ViewsSearchFieldBinding
-import com.ttcn.promotionsdk.ui.utils.PRMViewsDataProvider.getInputTextWatcher
 import com.ttcn.promotionsdk.ui.utils.enum.PRMSearchType
-import com.ttcn.promotionsdk.ui.utils.extension.getText
 import com.ttcn.promotionsdk.ui.utils.extension.hideSoftInput
-import com.ttcn.promotionsdk.ui.utils.extension.replaceAccents
 import com.ttcn.promotionsdk.ui.utils.extension.retrieveColor
 import com.ttcn.promotionsdk.ui.utils.extension.setFont
 import com.ttcn.promotionsdk.ui.utils.extension.showPRMSoftInput
@@ -29,6 +27,12 @@ class PRMSearchField @JvmOverloads constructor(
 ) : LinearLayout(context, attrs), View.OnFocusChangeListener, IPRMInput {
 
     val viewBinding = ViewsSearchFieldBinding.inflate(LayoutInflater.from(context), this, true)
+
+    private var onFocusListener: ((Boolean) -> Unit)? = null
+
+    fun setOnFocusListener(listener: (Boolean) -> Unit) {
+        onFocusListener = listener
+    }
 
     var hint: String = ""
         set(value) {
@@ -64,53 +68,81 @@ class PRMSearchField @JvmOverloads constructor(
             field = value
             if (padding != 0) {
                 viewBinding.searchInput.setPadding(
-                    resources.getDimension(R.dimen.tokenSpacing24).toInt(), padding, 0, padding
+                    resources.getDimension(com.intuit.sdp.R.dimen._30sdp).toInt(),
+                    padding,
+                    0,
+                    padding
                 )
             }
         }
 
     var onTextChangeListener: ((String) -> Unit)? = null
 
-    private val textWatcher by lazy { getInputTextWatcher() }
 
     private val textChangeListeners = mutableListOf<(String) -> Unit>()
 
-    var allowVietnamese: Boolean = false
+    private var previousKeyword: String = ""
 
     init {
-        viewBinding.searchInput.removeTextChangedListener(textWatcher)
-        viewBinding.searchInput.addTextChangedListener(textWatcher)
         viewBinding.searchInput.onFocusChangeListener = this
         viewBinding.buttonClear.setOnClickListener { viewBinding.searchInput.setText("") }
         if (attrs != null) {
             context.obtainStyledAttributes(attrs, R.styleable.PRMSearchField).apply {
-                hint = getString(R.styleable.PRMSearchField_prmSfHint).getText()
+                hint = getString(R.styleable.PRMSearchField_prmSfHint).toString()
                 val type = getInteger(R.styleable.PRMSearchField_prmSvType, 0)
                 searchType = PRMSearchType.values()[type % 3]
                 if (type == 2) {
-                    viewBinding.buttonSearch.visibility = VISIBLE
+                    viewBinding.buttonSearch.visibility = View.VISIBLE
                 }
-                allowVietnamese = getBoolean(R.styleable.PRMSearchField_prmSfAllowVietnamese, false)
                 recycle()
             }
+        }
+        viewBinding.searchInput.addTextChangedListener {
+            if (it.isNullOrBlank()) {
+                viewBinding.buttonClear.visibility = INVISIBLE
+            } else {
+                viewBinding.buttonClear.visibility = VISIBLE
+            }
+
+            val newKeyword = if (it.isNullOrBlank()) "" else it.toString().trim()
+
+            if (previousKeyword == newKeyword) {
+                return@addTextChangedListener
+            }
+
+            previousKeyword = newKeyword
+            onTextChangeListener?.invoke(newKeyword)
         }
     }
 
     private fun updateInputType() {
         val currentText = viewBinding.searchInput.text.toString().trimStart()
         searchType.apply {
+            viewBinding.searchIcon.setImageResource(iconRes)
+            val iconSize = resources.getDimension(iconSizeRes).toInt()
+            val params = viewBinding.searchIcon.layoutParams
+            params.width = iconSize
+            params.height = iconSize
+            viewBinding.searchIcon.layoutParams = params
             if (currentText.isEmpty()) {
-                viewBinding.buttonSearch.setTextColor(context.retrieveColor(R.color.color_222222))
-                viewBinding.buttonClear.visibility = INVISIBLE
+                viewBinding.buttonClear.visibility = View.INVISIBLE
             } else {
-                viewBinding.buttonClear.visibility = VISIBLE
-                viewBinding.buttonSearch.setTextColor(context.retrieveColor(R.color.color_EE0033))
+                viewBinding.buttonClear.visibility = View.VISIBLE
             }
             viewBinding.searchInput.apply {
                 viewBinding.searchInput.setFont(fontStyle)
                 setTextColor(context.retrieveColor(textColorRes))
                 setHintTextColor(context.retrieveColor(hintColorRes))
                 setBackgroundResource(if (hasFocus()) focusBackground else searchBackground)
+                if (searchType == PRMSearchType.NAVIGATION) {
+                    val currentBackground = background
+                    currentBackground.alpha = resources.getInteger(R.integer.tokenOpacity08)
+                    background = currentBackground
+                }
+                val leftPadding =
+                    iconSize + 2 * resources.getDimension(com.intuit.sdp.R.dimen._6sdp).toInt()
+                val rightPadding = viewBinding.buttonClear.layoutParams.width
+                setPadding(leftPadding, paddingTop, rightPadding, paddingBottom)
             }
         }
     }
@@ -123,31 +155,8 @@ class PRMSearchField @JvmOverloads constructor(
         return viewBinding.searchInput
     }
 
-    override fun onTextChanged(newString: String, oldString: String, isSelfUpdate: Boolean) {
-        var phrase = ""
-        if (isSelfUpdate) {
-            phrase = updateContent(newString)
-            if (phrase != newString) {
-                val length = viewBinding.searchInput.text.toString().length
-                viewBinding.searchInput.text?.replace(0, length, phrase)
-            }
-            if (phrase.isEmpty()) {
-                viewBinding.buttonSearch.setTextColor(context.retrieveColor(R.color.color_222222))
-                viewBinding.buttonClear.visibility = INVISIBLE
-            } else {
-                viewBinding.buttonSearch.setTextColor(context.retrieveColor(R.color.color_EE0033))
-                viewBinding.buttonClear.visibility = VISIBLE
-            }
-            val currentSelection = viewBinding.searchInput.selectionStart
-            val selection = getCursorPosition(currentSelection, phrase.length, oldString.length)
-            viewBinding.searchInput.setSelection(selection)
-            onTextChangeListener?.invoke(phrase)
-        }
-        super.onTextChanged(phrase, oldString, isSelfUpdate)
-    }
 
     fun setOnSearchActionListener(listener: OnClickListener?) {
-        hideSoftInput()
         viewBinding.buttonSearch.setOnClickListener(listener)
     }
 
@@ -164,18 +173,12 @@ class PRMSearchField @JvmOverloads constructor(
 
     override fun textChangeListeners(): MutableList<(String) -> Unit> = textChangeListeners
 
-    private fun updateContent(phrase: String): String {
-        var text = phrase.trimStart().replace("\\s+".toRegex(), " ")
-        if (maxLength > 0 && text.length > maxLength) {
-            text = text.substring(0, maxLength)
-        }
-        return if (allowVietnamese) text else text.replaceAccents()
-    }
-
     override fun onFocusChange(v: View, hasFocus: Boolean) {
         if (hasFocus) {
             v.showPRMSoftInput()
         }
         updateInputType()
+
+        onFocusListener?.invoke(hasFocus)
     }
 }
